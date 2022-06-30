85c60dcc2c4fa2f3079e3cfd3296509b167c913f
DHEERENDRA KUMAR SINGH <dhiru.all4u@gmail.com>
2022-06-30 09:00:32 +0530

    Add project files.

---
+++ System Design/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace System_Design
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
